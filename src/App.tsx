73b03416dff691bf60d90ee44699b03dfc566142
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2025-11-15 16:50:53 +0100

    fix: added the bargame page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import BarGame from "./pages/BarGame";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/bar/:barId" element={<BarGame />} />
+          <Route path="/game/:barId" element={<BarGame />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/BarGame.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BarGame = () => {
+  return (
+    <div>BarGame</div>
+  )
+}
+
+export default BarGame
